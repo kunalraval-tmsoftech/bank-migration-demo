d038e8764877785ad90ffec5f8615d7cec411479
kunalraval-tmsoftech <kunal.raval@tmsoftech.in>
2026-04-01 00:17:40 +0530

    Commi My Change

--- src/reconciliation/reconcile.js
+++ src/reconciliation/reconcile.js
@@ -1,1 +1,18 @@
+const pg = require('../db/postgres');
+
+async function reconcile() {
+    try {
+        const result = await pg.query(`SELECT COUNT(*) FROM bank_master`);
+
+        console.log("Postgres Count:", result.rows[0].count);
+
+        // Demo purpose
+        console.log("✅ Reconciliation Completed");
+
+    } catch (err) {
+        console.error("❌ Error:", err);
+    }
+}
+
+reconcile();
 
