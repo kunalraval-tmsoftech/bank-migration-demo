a49f6909bf5fce5b450b4677a5a5baa78b71a5c7
kunalraval-tmsoftech <kunal.raval@tmsoftech.in>
2026-04-01 00:08:35 +0530

    Commit My Changes

--- src/db/postgres.js
+++ src/db/postgres.js
@@ -1,1 +1,12 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'bankdb',
+    password: 'password',
+    port: 5432,
+});
+
+module.exports = pool;
 
