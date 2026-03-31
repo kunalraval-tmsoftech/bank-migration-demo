149e7d3c39b30dea67e7c47166476d35ae8adf16
kunalraval-tmsoftech <kunal.raval@tmsoftech.in>
2026-04-01 00:06:55 +0530

    Commit My Changes

--- src/db/oracle.js
+++ src/db/oracle.js
@@ -1,1 +1,12 @@
+const oracledb = require('oracledb');
+
+async function getOracleConnection() {
+    return await oracledb.getConnection({
+        user: "your_user",
+        password: "your_password",
+        connectString: "localhost/XEPDB1"
+    });
+}
+
+module.exports = { getOracleConnection };
 
