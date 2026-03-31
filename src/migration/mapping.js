00a35209e401b5616321357183aa93d459526c42
kunalraval-tmsoftech <kunal.raval@tmsoftech.in>
2026-04-01 00:10:56 +0530

    Commit My Changes

--- src/migration/mapping.js
+++ src/migration/mapping.js
@@ -1,1 +1,13 @@
+function mapToBankMaster(row) {
+    return {
+        bank_code: row.BANK_CODE,
+        bank_name: row.BANK_NAME,
+        bank_short_name_sms: row.BANK_SHRT_NM_SMS,
+        bankpara: row.BANKPARA,
+        sms_sender_bank: row.SMS_SENDER_BANK,
+        gst_no: row.GST_NO
+    };
+}
+
+module.exports = { mapToBankMaster };
 
