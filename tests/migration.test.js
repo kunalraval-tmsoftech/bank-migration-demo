5dbab9d0e2b2d87322a892f8a5ceb7a2cff8f232
kunalraval-tmsoftech <kunal.raval@tmsoftech.in>
2026-04-01 00:20:10 +0530

    Commit My Change

--- tests/migration.test.js
+++ tests/migration.test.js
@@ -1,1 +1,14 @@
+const { mapToBankMaster } = require('../src/migration/mapping');
+
+test('Mapping Test', () => {
+    const input = {
+        BANK_CODE: 1,
+        BANK_NAME: "SBI"
+    };
+
+    const output = mapToBankMaster(input);
+
+    expect(output.bank_code).toBe(1);
+    expect(output.bank_name).toBe("SBI");
+});
 
