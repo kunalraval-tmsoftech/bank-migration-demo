5d43c1264075a2b33fc1cf96f32bee6915e8e35f
kunalraval-tmsoftech <kunal.raval@tmsoftech.in>
2026-04-01 00:12:52 +0530

    Commit My Change

--- src/migration/migrate.js
+++ src/migration/migrate.js
@@ -1,1 +1,45 @@
+const { getOracleConnection } = require('../db/oracle');
+const pg = require('../db/postgres');
+const { mapToBankMaster } = require('./mapping');
+
+async function migrate() {
+    try {
+        const oracleConn = await getOracleConnection();
+
+        const result = await oracleConn.execute(`SELECT * FROM INFO`);
+
+        for (let row of result.rows) {
+
+            const mapped = mapToBankMaster(row);
+
+            await pg.query(
+                `INSERT INTO bank_master(
+                    bank_code,
+                    bank_name,
+                    bank_short_name_sms,
+                    bankpara,
+                    sms_sender_bank,
+                    gst_no
+                )
+                VALUES ($1,$2,$3,$4,$5,$6)
+                ON CONFLICT (bank_code) DO NOTHING`,
+                [
+                    mapped.bank_code,
+                    mapped.bank_name,
+                    mapped.bank_short_name_sms,
+                    mapped.bankpara,
+                    mapped.sms_sender_bank,
+                    mapped.gst_no
+                ]
+            );
+        }
+
+        console.log("✅ Migration Completed");
+
+    } catch (err) {
+        console.error("❌ Error:", err);
+    }
+}
+
+migrate();
 
